310fd3c64a04afd117535e9c35a002d9aff2d110
AmarWayadande <amarwayadande3142.com>
2022-06-02 15:54:49 +0530

    newerrore

--- A.java
+++ A.java
@@ -2,6 +2,11 @@ public class A {
 
     public static void main(String agrs)
     {
-      System.out.println("Amar");
+
+
+        System.out.println("Amar");
+        System.out.println("Amar");
     }
+
+
 }
