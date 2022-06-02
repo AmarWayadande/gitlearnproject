a4fa81ae241e9547d5ff36160657a012910fd461
AmarWayadande <amarwayadande3142.com>
2022-06-02 15:49:11 +0530

    .

--- A.java
+++ A.java
@@ -2,11 +2,6 @@ public class A {
 
     public static void main(String agrs)
     {
-
-
-        System.out.println("Amar");
-        System.out.println("Amar");
+      System.out.println("Amar");
     }
-
-
 }
